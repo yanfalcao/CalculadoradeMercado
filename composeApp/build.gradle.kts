1f5ba6b335d6f5c1b1bea024253224004c921dfd
yanfalcao <yanfalcao10@gmail.com>
2025-03-27 12:01:50 -0300

    Implement koin (dependence injection)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -31,6 +31,8 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.koin.android)
+            implementation(libs.koin.androidx.compose)
         }
         commonMain.dependencies {
             implementation(project(":core:designsystem"))
